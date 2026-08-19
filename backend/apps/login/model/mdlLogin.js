07ebfcc7d5d7dca8228d5f17dbe99b1c01ab83e3
André Pedronila <andreluiz.pedronila@gmail.com>
2026-08-19 10:07:14 -0300

    implementação pasta login

---
+++ backend/apps/login/model/mdlLogin.js
@@ -0,0 +1,16 @@
+//-- apps/login/model/mdlLogin.js
+
+const db = require("../../../database/databaseconfig");
+
+const GetCredencial = async (loginPar) => {
+    return (
+        await db.query(
+            "select username, password " +
+            "from usuarios where username = $1 and deleted = false", [loginPar]
+        )
+    ).rows;
+};
+//--$
+module.exports = {
+    GetCredencial,
+};
